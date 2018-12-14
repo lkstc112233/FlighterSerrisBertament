7452c9df41a962fee24efa51cea6eb9a472fc7cb
Photon cat <lkstc112233@users.noreply.github.com>
2018-12-14 16:02:44 -0500

    Added sprite manager

--- FlighterSerrisBertament/FlighterSerrisBertament/Sprite.cpp
+++ FlighterSerrisBertament/FlighterSerrisBertament/Sprite.cpp
@@ -1,3 +1,16 @@
 #include "stdafx.h"
 
 #include "Sprite.h"
+
+void SpriteManager::update() {
+  for (const auto& sprite : sprites) {
+    sprite->update();
+  }
+  sprites.remove_if([](const auto& s) { return s->isDead(); });
+}
+
+void SpriteManager::draw(ID2D1HwndRenderTarget* renderTarget) {
+  for (const auto& sprite : sprites) {
+    sprite->draw(renderTarget);
+  }
+}
--- FlighterSerrisBertament/FlighterSerrisBertament/Sprite.h
+++ FlighterSerrisBertament/FlighterSerrisBertament/Sprite.h
@@ -1,12 +1,22 @@
 #pragma once
+#include <list>
 
 class Sprite {
  public:
   Sprite() {}
   virtual ~Sprite() {}
-
- private:
   virtual bool isDead() { return false; }
   virtual void update() = 0;
-  virtual void draw(ID2D1HwndRenderTarget *renderTarget) = 0;
+  virtual void draw(ID2D1HwndRenderTarget* renderTarget) = 0;
+};
+
+class SpriteManager {
+ private:
+  std::list<std::unique_ptr<Sprite>> sprites;
+
+ public:
+  SpriteManager() {}
+  ~SpriteManager() {}
+  void update();
+  void draw(ID2D1HwndRenderTarget* renderTarget);
 };
