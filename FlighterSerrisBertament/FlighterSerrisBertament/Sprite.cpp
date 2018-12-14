ce9f0edb6c04d242d5c8e770f3742b7de09f198e
Photon cat <lkstc112233@users.noreply.github.com>
2018-12-14 15:05:05 -0500

    Made sprite an interface.

--- FlighterSerrisBertament/FlighterSerrisBertament/Sprite.cpp
+++ FlighterSerrisBertament/FlighterSerrisBertament/Sprite.cpp
@@ -1,7 +1,3 @@
 #include "stdafx.h"
 
 #include "Sprite.h"
-
-Sprite::Sprite() {}
-
-Sprite::~Sprite() {}
--- FlighterSerrisBertament/FlighterSerrisBertament/Sprite.h
+++ FlighterSerrisBertament/FlighterSerrisBertament/Sprite.h
@@ -2,6 +2,11 @@
 
 class Sprite {
  public:
-  Sprite();
-  ~Sprite();
+  Sprite() {}
+  virtual ~Sprite() {}
+
+ private:
+  virtual bool isDead() { return false; }
+  virtual void update() = 0;
+  virtual void draw(ID2D1HwndRenderTarget *renderTarget) = 0;
 };
