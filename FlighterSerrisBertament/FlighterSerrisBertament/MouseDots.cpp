55cbdc942bc4b7396cdbb30ea2655e4ed42c8868
Photon cat <lkstc112233@users.noreply.github.com>
2018-12-21 05:03:22 -0500

    Fixed an issue in mouse dot

--- FlighterSerrisBertament/FlighterSerrisBertament/MouseDots.cpp
+++ FlighterSerrisBertament/FlighterSerrisBertament/MouseDots.cpp
@@ -16,16 +16,16 @@ void MouseDots::update(float time) {
   float dy = mouse->getY() - y;
 
   // Calculate rate
-  float rate = sqrt(1 / (dx * dx + dy * dy)) / speed;
+  float rate = 1 / sqrt((dx * dx + dy * dy)) / speed;
 
   // Move the dot
-  if (rate > speed) {
-    x += dx / rate;
-    y += dy / rate;
+  if (rate < 1 / speed) {
+    x += dx * rate;
+    y += dy * rate;
   } else {
     // If near enough, only half the move is performed.
-    x += dx / rate / 2;
-    x += dx / rate / 2;
+    x += dx * rate / 2;
+    y += dx * rate / 2;
   }
 }
 
