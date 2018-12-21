5ac48ded97bd782913b29cc279db5203836a78d1
Photon cat <lkstc112233@users.noreply.github.com>
2018-12-21 05:04:04 -0500

    Formatation change.

--- FlighterSerrisBertament/FlighterSerrisBertament/utility.h
+++ FlighterSerrisBertament/FlighterSerrisBertament/utility.h
@@ -1,6 +1,6 @@
 #pragma once
 template <class Interface>
-inline void SafeRelease(Interface **ppInterfaceToRelease) {
+inline void SafeRelease(Interface** ppInterfaceToRelease) {
   if (*ppInterfaceToRelease != NULL) {
     (*ppInterfaceToRelease)->Release();
 
