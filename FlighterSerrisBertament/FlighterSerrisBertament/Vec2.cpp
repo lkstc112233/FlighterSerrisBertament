ff55250a7f7a502234d4f20c2edebff848ed321a
Photon cat <lkstc112233@users.noreply.github.com>
2018-12-21 17:37:48 -0500

    Added a vector class stub

---
+++ FlighterSerrisBertament/FlighterSerrisBertament/Vec2.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+
+#include "Vec2.h"
+
+Vec2::Vec2(float both = 0.0F) : x(both), y(both) {}
+
+Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
+
+Vec2::~Vec2() {}
---
+++ FlighterSerrisBertament/FlighterSerrisBertament/Vec2.h
@@ -0,0 +1,9 @@
+#pragma once
+class Vec2 {
+ public:
+  float x;
+  float y;
+  Vec2(float both = 0.0F);
+  Vec2(float x, float y);
+  ~Vec2();
+};
